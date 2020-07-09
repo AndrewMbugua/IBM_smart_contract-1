017ab08c8f584a7401333c56566b294b7aa785c3
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-07-09 16:17:53 +0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const my_asset_contract_1 = require("./my-asset-contract");
+var my_asset_contract_2 = require("./my-asset-contract");
+exports.MyAssetContract = my_asset_contract_2.MyAssetContract;
+exports.contracts = [my_asset_contract_1.MyAssetContract];
+//# sourceMappingURL=index.js.map
